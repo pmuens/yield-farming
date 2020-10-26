4e867a2d1047152eee00f66750fdc2b82ec3f5a6
Philipp Muens <raute1337@gmx.de>
2020-10-26 12:45:49 +0100

    Downgrade Solidity compiler version

--- truffle-config.js
+++ truffle-config.js
@@ -13,7 +13,7 @@ module.exports = {
   contracts_build_directory: "./src/abis/",
   compilers: {
     solc: {
-      version: "0.7.4",
+      version: "0.7.0",
       optimizer: {
         enabled: true,
         runs: 200,
