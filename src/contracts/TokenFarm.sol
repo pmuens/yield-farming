de9c964de686f7fcf495a8bfa88a155607f3f184
Philipp Muens <raute1337@gmx.de>
2020-10-26 13:15:00 +0100

    Deploy and distribute tokens

--- src/contracts/TokenFarm.sol
+++ src/contracts/TokenFarm.sol
@@ -1,6 +1,16 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.4.22 <0.8.0;
 
+import "./DappToken.sol";
+import "./DaiToken.sol";
+
 contract TokenFarm {
   string public name = "Dapp Token Farm";
+  DappToken public dappToken;
+  DaiToken public daiToken;
+
+  constructor(DappToken _dappToken, DaiToken _daiToken) {
+    dappToken = _dappToken;
+    daiToken = _daiToken;
+  }
 }
